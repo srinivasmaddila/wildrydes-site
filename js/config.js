10b53c14d7f9db2c9a70c7bc3e2300619d7a50e5
srinivas <maddilasrinivas06@gmail.com>
2025-05-25 02:05:05 +0530

    Update config.js
    changes2

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_G5dDWg1sI', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7706mpvi33sdlgl3ebhgsckgco', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
