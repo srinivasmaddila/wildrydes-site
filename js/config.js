0530fcd2390a5683f90c7be0aea21ccdcb70ad29
srinivas <maddilasrinivas06@gmail.com>
2025-05-25 02:03:33 +0530

    Update config.js
    changesd

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_G5dDWg1sI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7706mpvi33sdlgl3ebhgsckgco', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
